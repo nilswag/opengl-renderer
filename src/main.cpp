e7e2c6489ef7848c1ebc3e35de408b0704caa87e
nilswag <nilswagemaker05@icloud.com>
2026-05-11 14:45:14 +0200

    Vulkan template

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,5 @@
-#include <stdexcept>
-#include <iostream>
-#include <print>
-
-#include "app.hpp"
-
-namespace engine
-{
-	App::App()
-		: 
-		window(WIDTH, HEIGHT, "Vulkan Window"),
-		pipeline("shaders/basic.vert.spv", "shaders/basic.frag.spv")
-	{ }
-
-	void App::run()
-	{
-		while (!window.should_close())
-		{
-			window.poll_events();
-		}
-	}
-}
 
 int main()
 {
-	try
-	{
-		engine::App app;
-		app.run();
-	} catch (const std::exception& e)
-	{
-		std::println(std::cerr, "Error: {}", e.what());
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS;
+	return 0;
 }
