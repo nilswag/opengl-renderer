f57212864e90ff927edd286945176b84d79155a1
nilswag <nilswagemaker05@icloud.com>
2026-05-16 00:06:09 +0200

    Added shader class header

---
+++ src/gfx/shader.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <glad/gl.h>
+
+class Shader
+{
+public:
+	Shader(const std::string& vertexPath, const std::string& fragmentPath);
+	~Shader();
+
+	inline void use() const { glUseProgram(program); }
+	inline GLuint getPrograM() const { return program; }
+
+private:
+	GLuint compileShader(const std::string& path, GLenum type);
+
+	GLuint program;
+};
