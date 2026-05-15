c64470f83a9e4cf0a0ae63912807d68838ea0e45
nilswag <nilswagemaker05@icloud.com>
2026-05-16 00:27:24 +0200

    Added shader compilation check

--- src/gfx/shader.cpp
+++ src/gfx/shader.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <stdexcept>
 #include <glad/gl.h>
 #include "shader.hpp"
 #include "../util/io.h"
@@ -29,6 +30,19 @@ GLuint Shader::compileShader(const std::string& src, GLenum type)
 	const char* str = src.c_str();
 	glShaderSource(shader, 1, &str, nullptr);
 	glCompileShader(shader);
+
+	GLint success;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		std::string log;
+		GLint length;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+		log.resize(length);
+
+		glGetShaderInfoLog(shader, log.size(), nullptr, log.data());
+		throw std::runtime_error("Could not compile shader: " + log);
+	}
 
 	return shader;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
+#include <stdexcept>
+#include <print>
 #include <glad/gl.h>
 #include "gfx/window.hpp"
+#include "gfx/shader.hpp"
 
 int main()
 {
-	Window window(800, 800, "OpenGL");
-
-	while (!window.shouldClose())
+	try
 	{
-		window.pollEvents();
+		Window window(800, 800, "OpenGL");
+		Shader shader("shaders/vertex.glsl", "shaders/fragment.glsl");
 
-		float color[3] = { 0.0f };
-		glClearNamedFramebufferfv(0, GL_COLOR, 0, color);
-		window.swapBuffers();
+		while (!window.shouldClose())
+		{
+			window.pollEvents();
+
+			float color[3] = { 0.0f };
+			glClearNamedFramebufferfv(0, GL_COLOR, 0, color);
+			window.swapBuffers();
+		}
+	}
+	catch (const std::exception& e)
+	{
+		std::println("{}", e.what());
 	}
 
 	return 0;
