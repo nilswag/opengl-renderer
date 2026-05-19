db72c1ce5aeac807200f88da44be0363d53ad19e
nilswag <nilswagemaker05@icloud.com>
2026-05-19 18:02:14 +0200

    Made renderer non-copyable and non-movable

--- src/gfx/renderer.hpp
+++ src/gfx/renderer.hpp
@@ -4,4 +4,10 @@ class Renderer
 {
 public:
 	Renderer();
+	~Renderer();
+
+	Renderer(const Renderer&) = delete;
+	Renderer& operator=(const Renderer&) = delete;
+	Renderer(Renderer&&) = delete;
+	Renderer& operator=(Renderer&&) = delete;
 };
